13d22ccb31cd4de0a8b15ed6cffb57e5c3d5703f
kaiawilson <aionmone14@gmail.com>
2022-09-30 15:46:55 +0200

    Add basic theme colors

---
+++ packages/core/src/colors.js
@@ -0,0 +1,10 @@
+const { blue, gray, green, red, amber, indigo } = require("@ideoui/colors");
+
+module.exports = {
+  primary: { ...blue },
+  secondary: { ...gray },
+  success: { ...green },
+  danger: { ...red },
+  warning: { ...amber },
+  info: { ...indigo },
+};
